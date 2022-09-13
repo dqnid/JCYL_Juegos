3e75f82ec08af0aa8e5c29e259d3a10e9230a0ae
dqnid <dani.heras@hotmail.com>
2022-09-13 08:29:52 +0200

    Marcadores añadidos al mapa

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var map = L.map('map').setView([41.96025655115759,-2.392389852116968], 8);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+const basemaps = {
+    StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
+    Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
+    Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'})
+};
+L.control.layers(basemaps).addTo(map);
+
+const json = require("./data/zona_rec.json");
+for (let key in json){
+    console.log(json[key].nombre);
+    L.marker([json[key].lat,json[key].lng]).addTo(map);
+}
