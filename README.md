49ada4862c91204dd9828b498ec1cf4dd9d3ba21
dqnid <dani.heras@hotmail.com>
2022-09-19 17:55:28 +0200

    Limpieza

--- README.md
+++
@@ -1,5 +0,0 @@
-# JCYL_Juegos
-Proyecto de concurso público de la junta de Castilla y León sobre el uso de datos abiertos.
-Miembros del proyecto:
-  - Carlos
-  - Daniel
--- scripts.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "15px 70px";
-  } else {
-    document.getElementById("navbar").style.padding = "35px 70px";
-  }
-} 
