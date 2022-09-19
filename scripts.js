8bbec11bb2149eebcd445cf53a3ce9c4e8325781
dqnid <dani.heras@hotmail.com>
2022-09-19 13:42:00 +0200

    Añadidas imágenes y tarjetas de juegos.

--- scripts.js
+++ scripts.js
@@ -2,10 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.height = "6%";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.padding = "15px 70px";
   } else {
-    document.getElementById("navbar").style.height = "10%";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "35px 70px";
   }
 } 
